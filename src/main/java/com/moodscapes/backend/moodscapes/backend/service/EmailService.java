7619c91eebf01f34ec1f9ffcccfc7c74f532a434
sebastine <chiemelienwobodo8@gmail.com>
2024-05-31 17:17:15 +0100

    dependency injection on javamail in EmailService

--- src/main/java/com/moodscapes/backend/moodscapes/backend/service/EmailService.java
+++ src/main/java/com/moodscapes/backend/moodscapes/backend/service/EmailService.java
@@ -2,8 +2,19 @@ package com.moodscapes.backend.moodscapes.backend.service;
 
 import com.moodscapes.backend.moodscapes.backend.entity.Auth;
 import com.moodscapes.backend.moodscapes.backend.service.interfaces.IEmailService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class EmailService implements IEmailService {
+
+    private final JavaMailSender mailSender;
+    private String host;
+
     @Override
     public void sendMagicTokenMail(Auth token) {
 
