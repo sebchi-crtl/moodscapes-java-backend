1201fad683add2283ef51295bf561d568df7bb37
sebastine <chiemelienwobodo8@gmail.com>
2024-05-31 16:19:23 +0100

    Update .gitignore to exclude .idea/workspace.xml

---
+++ src/main/java/com/moodscapes/backend/moodscapes/backend/service/IMagicService.java
@@ -0,0 +1,26 @@
+package com.moodscapes.backend.moodscapes.backend.service;
+
+import com.moodscapes.backend.moodscapes.backend.dao.AuthRepo;
+import com.moodscapes.backend.moodscapes.backend.entity.Auth;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+import java.security.SecureRandom;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static com.moodscapes.backend.moodscapes.backend.enumeration.SignInMethod.MagicLink;
+
+public interface IMagicService {
+
+    public String token();
+
+    public void issueToken(String username);
+
+
+}
