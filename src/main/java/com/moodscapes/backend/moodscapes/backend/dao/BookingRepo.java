28390a6fc49559e19fec4768d228ec6608770708
sebastine <chiemelienwobodo8@gmail.com>
2024-06-02 04:36:41 +0100

    removed Long to String in rep repo

--- src/main/java/com/moodscapes/backend/moodscapes/backend/dao/BookingRepo.java
+++ src/main/java/com/moodscapes/backend/moodscapes/backend/dao/BookingRepo.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BookingRepo extends JpaRepository<Bookings, Long> {
+public interface BookingRepo extends JpaRepository<Bookings, String> {
 }
--- src/main/java/com/moodscapes/backend/moodscapes/backend/dao/GuestRepo.java
+++ src/main/java/com/moodscapes/backend/moodscapes/backend/dao/GuestRepo.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GuestRepo extends JpaRepository<Guest, Long> {
+public interface GuestRepo extends JpaRepository<Guest, String> {
 }
