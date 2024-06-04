0b75d3e730301ee9adbb8e26c797ad6f7ffb01c7
sebastine <chiemelienwobodo8@gmail.com>
2024-06-04 18:58:13 +0100

    removed EventResponseDto

--- src/main/java/com/moodscapes/backend/moodscapes/backend/mapper/UserMapper.java
+++ src/main/java/com/moodscapes/backend/moodscapes/backend/mapper/UserMapper.java
@@ -1,25 +1,28 @@
 package com.moodscapes.backend.moodscapes.backend.mapper;
 
 import com.moodscapes.backend.moodscapes.backend.dto.response.EventResponseDTO;
+import com.moodscapes.backend.moodscapes.backend.dto.response.UserResponseDTO;
 import com.moodscapes.backend.moodscapes.backend.entity.Event;
+import com.moodscapes.backend.moodscapes.backend.entity.User;
 import org.springframework.stereotype.Service;
 
 import java.util.function.Function;
 
 @Service
-public class UserMapper implements Function<Event, EventResponseDTO> {
-
+public class UserMapper implements Function<User, UserResponseDTO> {
 
     @Override
-    public EventResponseDTO apply(Event event) {
-        return new EventResponseDTO(
-                event.getEventId(),
-                event.getUserId(),
-                event.getTitle(),
-                event.getLocation(),
-                event.isAvailable(),
-                event.getNote(),
-                event.getCreatedAt()
+    public UserResponseDTO apply(User user) {
+        return new UserResponseDTO(
+                user.getId(),
+                user.getEmail(),
+                user.getFullName(),
+                user.getBio(),
+                user.getPhoneNumber(),
+                user.isEnabled(),
+                user.getImageUrl(),
+                user.getRole(),
+                user.getAddress()
 
         );
     }
