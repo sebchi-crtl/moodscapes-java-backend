27252877c955477087e36181d2c8ee770ec031d0
sebastine <chiemelienwobodo8@gmail.com>
2024-06-04 18:51:40 +0100

    update on UserResponseDTO

--- src/main/java/com/moodscapes/backend/moodscapes/backend/dto/response/UserResponseDTO.java
+++ src/main/java/com/moodscapes/backend/moodscapes/backend/dto/response/UserResponseDTO.java
@@ -1,14 +1,20 @@
 package com.moodscapes.backend.moodscapes.backend.dto.response;
 
+import com.moodscapes.backend.moodscapes.backend.enumeration.Role;
+
 import java.time.LocalDateTime;
+import java.util.Set;
 
 public record UserResponseDTO(
         String id,
-        String userId,
-        String title,
-        String location,
-        boolean available,
-        String note,
-        LocalDateTime createdAt
+        String email,
+        String fullName,
+        String bio,
+        Set<String> phoneNumber,
+        boolean enabled,
+        String imageUrl,
+        Set<Role> role,
+        String address
 ) {
+
 }
