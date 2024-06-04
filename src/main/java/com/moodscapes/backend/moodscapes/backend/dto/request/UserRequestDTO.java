2ece8d250d4c0c96199437113ce7124c6797472e
sebastine <chiemelienwobodo8@gmail.com>
2024-06-04 18:52:49 +0100

    proper parameters on UserRequestDTO

--- src/main/java/com/moodscapes/backend/moodscapes/backend/dto/request/UserRequestDTO.java
+++ src/main/java/com/moodscapes/backend/moodscapes/backend/dto/request/UserRequestDTO.java
@@ -1,11 +1,18 @@
 package com.moodscapes.backend.moodscapes.backend.dto.request;
 
+import com.moodscapes.backend.moodscapes.backend.enumeration.Role;
+
+import java.util.Set;
+
 public record UserRequestDTO(
-        String title,
-        String author,
-        boolean available
+        String email,
+        String fullName,
+        Set<String> phoneNumber,
+        String imageUrl,
+        Set<Role> role,
+        String address
 ) {
-    public static UserRequestDTO of(String title, String author, boolean available) {
+    public static UserRequestDTO of(String email, String fullName, Set<String> phoneNumber, String imageUrl, Set<Role> role, String address) {
         return null;
     }
 }
