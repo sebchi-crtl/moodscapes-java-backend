8e0a4727407afe6d1b3360d93a166556fdaacddc
sebastine <chiemelienwobodo8@gmail.com>
2024-06-04 18:30:42 +0100

    created Constants

---
+++ src/main/java/com/moodscapes/backend/moodscapes/backend/constant/Constants.java
@@ -0,0 +1,6 @@
+package com.moodscapes.backend.moodscapes.backend.constant;
+
+public class Constants {
+    public static final String USER_BIO = "Please add a little biography of yourself or company";
+
+}
