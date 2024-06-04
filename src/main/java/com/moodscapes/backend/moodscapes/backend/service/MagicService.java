8be1868af70f6f2ea88671caffb6bc533f297bd4
sebastine <chiemelienwobodo8@gmail.com>
2024-06-04 19:01:29 +0100

    reconstruct the methods

--- src/main/java/com/moodscapes/backend/moodscapes/backend/service/MagicService.java
+++ src/main/java/com/moodscapes/backend/moodscapes/backend/service/MagicService.java
@@ -4,16 +4,12 @@ import com.moodscapes.backend.moodscapes.backend.repository.AuthRepo;
 import com.moodscapes.backend.moodscapes.backend.entity.Auth;
 import com.moodscapes.backend.moodscapes.backend.service.interfaces.IEmailService;
 import com.moodscapes.backend.moodscapes.backend.service.interfaces.IMagicService;
+import com.moodscapes.backend.moodscapes.backend.service.interfaces.IUserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
-import java.security.SecureRandom;
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 import static com.moodscapes.backend.moodscapes.backend.enumeration.SignInMethod.MagicLink;
 
@@ -23,28 +19,8 @@ import static com.moodscapes.backend.moodscapes.backend.enumeration.SignInMethod
 public class MagicService extends AuthService implements IMagicService{
 
     private final AuthRepo auth;
+    private IUserService userService;
     private final IEmailService emailService;
-    private static final SecureRandom random = new SecureRandom();
-    private static final List<Character> alphabet =
-            IntStream.concat(
-                            IntStream.rangeClosed('a', 'z'),
-                            IntStream.concat(
-                                    IntStream.rangeClosed('A', 'Z'),
-                                    IntStream.rangeClosed('0', '9')
-                            )
-                    )
-                    .mapToObj(c -> (char) c)
-                    .collect(Collectors.toList());
-
-
-    private static String token(int size){
-        return IntStream
-                .range(0, size)
-                .map(i -> random.nextInt(alphabet.size()))
-                .mapToObj(alphabet::get)
-                .map(String::valueOf)
-                .collect(Collectors.joining());
-    }
 
     @Override
     public String token() {
@@ -52,20 +28,42 @@ public class MagicService extends AuthService implements IMagicService{
     }
 
     @Override
-    public void issueToken(String username){
+    public String issueToken(String email){
 //        Todo: import this to auth service with exceptions and try catch
         var token = token();
+        return token;
+    }
+
+    @Override
+    public void sendAuthentication(String email) {
+        try{
+
+            String token = issueToken(email);
+            Auth magic = saveToken(email, token);
+            if (userService.getUserByEmail(email))
+                emailService.sendMagicTokenMail(magic);
+
+            else
+                emailService.sendMagicTokenMailToNewUser(magic);
+
+        }catch (Exception ex){
+
+        }
+
+
+    }
+    @Override
+    protected Auth saveToken(String email, String token) {
         Auth magic = auth.save(
                 Auth
                         .builder()
-                        .email(username)
+                        .email(email)
                         .token(token)
                         .methodProvider(MagicLink)
                         .createdAt(LocalDateTime.now())
                         .build()
         );
-        emailService.sendMagicTokenMail(magic);
-
+        return magic;
     }
 
 }
