4904f92717f3f249c40bb6d5efd4639d5d8bcfae
sebastine <chiemelienwobodo8@gmail.com>
2024-06-10 20:55:45 +0100

    changed enabled to false

--- src/main/java/com/moodscapes/backend/moodscapes/backend/util/UserUtils.java
+++ src/main/java/com/moodscapes/backend/moodscapes/backend/util/UserUtils.java
@@ -1,13 +1,13 @@
 package com.moodscapes.backend.moodscapes.backend.util;
 
-import com.moodscapes.backend.moodscapes.backend.domain.RequestContext;
 import com.moodscapes.backend.moodscapes.backend.entity.User;
 import com.moodscapes.backend.moodscapes.backend.enumeration.Role;
-import static org.apache.commons.lang3.StringUtils.EMPTY;
 
 import java.util.Collections;
 import java.util.Set;
 import java.util.UUID;
+
+import static org.apache.commons.lang3.StringUtils.EMPTY;
 
 public class UserUtils {
 
@@ -21,7 +21,7 @@ public class UserUtils {
                 .fullName(fullName)
                 .bio(EMPTY)
                 .phoneNumber(Collections.singleton(EMPTY))
-                .enabled(true)
+                .enabled(false)
                 .imageUrl("https://upload.wikimedia.org/wikipedia/commons/6/67/User_Avatar.png")
                 .role(role)
                 .address(EMPTY)
