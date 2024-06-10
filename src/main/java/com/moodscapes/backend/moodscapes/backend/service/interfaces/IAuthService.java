2c0792f71b55280d1920fc24698ed9f7941216b2
sebastine <chiemelienwobodo8@gmail.com>
2024-06-10 13:57:08 +0100

    modification interface

--- src/main/java/com/moodscapes/backend/moodscapes/backend/service/interfaces/IAuthService.java
+++ src/main/java/com/moodscapes/backend/moodscapes/backend/service/interfaces/IAuthService.java
@@ -1,7 +1,17 @@
 package com.moodscapes.backend.moodscapes.backend.service.interfaces;
 
+import com.moodscapes.backend.moodscapes.backend.dto.request.UserRequestDTO;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.transaction.Transactional;
+
 public interface IAuthService {
 
-    void signInWithMagicLink(String email);
+    void sendMagicLinkToken(String email);
+
+    void signInWithMagicLink(String token, UserRequestDTO userRequestDTO);
+
     void signInWithGoogleOAuth(String email);
+//    @Transactional
+//    void authenticate(String email, HttpServletRequest request, HttpServletResponse response);
 }
