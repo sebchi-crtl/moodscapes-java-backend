d9cd0234473b355da956750baf8db49d3041a480
sebastine <chiemelienwobodo8@gmail.com>
2024-06-10 13:54:50 +0100

    User findUserByEmail(String email);

--- src/main/java/com/moodscapes/backend/moodscapes/backend/service/interfaces/IUserService.java
+++ src/main/java/com/moodscapes/backend/moodscapes/backend/service/interfaces/IUserService.java
@@ -3,11 +3,11 @@ package com.moodscapes.backend.moodscapes.backend.service.interfaces;
 import com.moodscapes.backend.moodscapes.backend.dto.request.UserRequestDTO;
 import com.moodscapes.backend.moodscapes.backend.entity.User;
 
-import java.util.Optional;
-
 public interface IUserService {
 
     void createUser(UserRequestDTO request);
 
     boolean getUserByEmail(String email);
+
+    User findUserByEmail(String email);
 }
