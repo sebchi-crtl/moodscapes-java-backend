4dd4b616ca8cc60bad683cfa2e6c3e19328feddb
sebastine <chiemelienwobodo8@gmail.com>
2024-06-10 13:59:50 +0100

    modification service

--- src/main/java/com/moodscapes/backend/moodscapes/backend/service/AuthService.java
+++ src/main/java/com/moodscapes/backend/moodscapes/backend/service/AuthService.java
@@ -1,16 +1,18 @@
 package com.moodscapes.backend.moodscapes.backend.service;
 
+import com.moodscapes.backend.moodscapes.backend.dto.request.UserRequestDTO;
 import com.moodscapes.backend.moodscapes.backend.entity.Auth;
+import com.moodscapes.backend.moodscapes.backend.exception.ApiException;
 import com.moodscapes.backend.moodscapes.backend.service.interfaces.IAuthService;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
 
 import java.security.SecureRandom;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+@Slf4j
 public abstract class AuthService implements IAuthService {
 
     private static final SecureRandom random = new SecureRandom();
@@ -41,8 +43,25 @@ public abstract class AuthService implements IAuthService {
         return null;
     }
 
+    @Transactional
+    protected abstract void authenticate(String token, UserRequestDTO userRequestDTO);
+
+
     @Override
-    public void signInWithMagicLink(String email) {
+    public void signInWithMagicLink(String token, UserRequestDTO userRequestDTO) {
+        try {
+            System.out.println("did the token come in here? " + token);
+            log.info("did your user request reach here? " + userRequestDTO);
+            authenticate(token, userRequestDTO);
+        }
+        catch(Exception ex){
+            throw new ApiException(ex.getMessage());
+        }
+
+    }
+
+    @Override
+    public void sendMagicLinkToken(String email) {
         try {
             System.out.println("memem");
             sendAuthentication(email);
@@ -65,6 +84,4 @@ public abstract class AuthService implements IAuthService {
         }
     }
 
-    @Transactional
-    protected abstract void authenticate(String token, HttpServletRequest request, HttpServletResponse response);
 }
